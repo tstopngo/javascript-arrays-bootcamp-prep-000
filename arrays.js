24e7ea7c20c5b9494340a2b25ffab53f3914c089
Tracy Ngo <tngo1206@gmail.com>
2019-02-01 04:19:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -27,6 +27,13 @@ function destructivelyRemoveElementFromBeginningOfArray (array){
 }
 
 function removeElementFromBeginningOfArray(array) {
- return array.slice(0)
-  
+ return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  return array.pop()
+}
+function removeElementFromEndOfArray(array){
+  return array.slice(0,array.length-1)
+}
 }
